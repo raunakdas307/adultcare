a39f2c0154c3c7314b83a9b8810bd9a02d37321b
raunakdas307 <raunakdas307@gmail.com>
2025-09-01 16:27:44 +0530

    Update frontend to use deployed backend API

--- adultcare-frontend/src/api.js
+++ adultcare-frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://127.0.0.1:8000/", // Django backend
+  baseURL: "https://adultcare-backend.onrender.com",
 });
 
 // Attach access token to every request
